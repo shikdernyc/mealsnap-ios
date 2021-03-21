abbb1e5ebaa65c2aaa2c55d8763035bfdcf61e42
Asif Shikder <shikdernyc@gmail.com>
2021-03-21 19:35:34 -0400

    Initial App

---
+++ MealSnap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MealSnap
+//
+//  Created by Asif Shikder on 3/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
