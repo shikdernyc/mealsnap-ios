4a0b979bdd6eac7c351f5d329635fb860e9f5fef
Asif Shikder <shikdernyc@gmail.com>
2021-04-07 19:13:14 -0400

    Handling signup controller logic

--- MealSnap/ViewControllers/SignupViewController.swift
+++ MealSnap/ViewControllers/SignupViewController.swift
@@ -8,21 +8,70 @@
 import UIKit
 
 class SignupViewController: UIViewController {
-    public static let StoryboardId = "signup_vc"
-
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    func showError(message: String) -> Void {
+        DispatchQueue.main.async {
+            print("Showing error message")
+            self.errorMessageLabel.text = message
+            self.errorMessageLabel.isHidden = false
+        }
+    }
+    
+    func closeErrorMessage() -> Void {
+        DispatchQueue.main.async {
+            if self.errorMessageLabel.isHidden == false {
+                self.errorMessageLabel.isHidden = true
+            }
+        }
+    }
+    
     @IBAction func handlePressSignup() {
-        guard let rootAuthController = storyboard?.instantiateViewController(identifier: "root_tab_vc") else {
+        print("Handling signup")
+        self.closeErrorMessage()
+        guard usernameTextField.text?.count != 0 else {
+            showError(message: "Username is required")
             return
         }
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.setRootViewController(controller: rootAuthController)
+        guard emailTextField.text?.count != 0 else {
+            self.showError(message: "Email is required")
+            return
+        }
+        guard passwordTextField.text?.count != 0 else {
+            self.showError(message: "Password is required")
+            return
+        }
+        print("Trying to signup")
+        AuthManager.signup(email: emailTextField.text!, username: usernameTextField.text!, password: passwordTextField.text!) { result in
+                switch result {
+                case.failure(let error):
+                    // TODO: Set Error Message
+                    print("error happened")
+                    print(error)
+                    print("Description: \(error.errorDescription)")
+                    self.showError(message: error.errorDescription)
+                    return
+                case .success:
+                    DispatchQueue.main.async {
+                        guard let onboardingVC = self.storyboard?.instantiateViewController(identifier: StoryboardId.SignupConfirmationViewController.rawValue) else {
+                            return
+                        }
+                        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.setRootViewController(controller: onboardingVC)
+                    }
+                    return
+            }
+        }
     }
+}
     /*
     // MARK: - Navigation
 
@@ -33,4 +82,3 @@ class SignupViewController: UIViewController {
     }
     */
 
-}
