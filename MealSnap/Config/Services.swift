a359626974070e15b64c54b56bdd038f93b7e0d1
Asif Shikder <shikdernyc@gmail.com>
2021-04-08 17:33:00 -0400

    Added architecture for posting photos, including select photo, title and description of photo

---
+++ MealSnap/Config/Services.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  MealSnap
+//
+//  Created by Asif Shikder on 4/8/21.
+//
+
+import Foundation
---
+++ MealSnap/Models/Image/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  MealSnap
+//
+//  Created by Asif Shikder on 4/8/21.
+//
+
+import Foundation
