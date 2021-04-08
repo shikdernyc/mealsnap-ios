401d7f7cadc171a187f1b35eb1e924c17bd74cd8
Asif Shikder <shikdernyc@gmail.com>
2021-04-08 17:34:20 -0400

    Created View Controller for Create Post

--- MealSnap/ViewControllers/CreatePostViewController.swift
+++ MealSnap/ViewControllers/CreatePostViewController.swift
@@ -8,22 +8,65 @@
 import UIKit
 
 class CreatePostViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var titleInput: UITextField!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var imagePreviewImageView: UIImageView!
+    
+    func styleInput(view: UIView) {
+        view.layer.borderWidth = 0.3
+        view.layer.cornerRadius = 7
+        view.layer.masksToBounds = true
+        view.layer.borderColor = UIColor.gray.cgColor
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        descriptionTextView
+        styleInput(view: descriptionTextView)
+        styleInput(view: titleInput)
+        descriptionTextView.layer.borderColor = UIColor.gray.cgColor
+        self.openImagePicker()
     }
-    */
+    
+    @IBAction func handleSave() {
+        print("Trying to save")
+        let imageData = imagePreviewImageView.image?.jpegData(compressionQuality: 1)?.base64EncodedString()
+    }
+    
+    func openImagePicker() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        if UIImagePickerController.isSourceTypeAvailable(.camera){
+            picker.sourceType = .camera
+        }else{
+            picker.sourceType = .photoLibrary
+        }
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func cancelUpload() {
+        self.navigationController?.popViewController(animated: true)
+    }
 
+}
+
+extension CreatePostViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: true, completion: nil)
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage else {
+            self.cancelUpload()
+            return
+        }
+        imagePreviewImageView.image = image
+        imagePreviewImageView.contentMode = .scaleAspectFill
+        imagePreviewImageView.layer.cornerRadius = 7
+        return
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true)
+        self.cancelUpload()
+    }
 }
