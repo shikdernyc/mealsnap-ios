08000f2d0ab70a980a5e77592d022197d6337c6a
Asif Shikder <shikdernyc@gmail.com>
2021-04-07 23:23:50 -0400

    Added Onboarding view controller. Added Settings View Controller with Logout

--- MealSnap/ViewControllers/Onboarding.swift
+++ MealSnap/ViewControllers/Onboarding.swift
@@ -6,39 +6,17 @@
 //
 
 import UIKit
+import Amplify
 
-class OnboardingViewController: UIViewController, AuthStateChangeHandler {
-    func onAuthStateChange(isAuthenticated: Bool) {
-        print("Onboarding Auth State: \(isAuthenticated)")
-        if(isAuthenticated){
-            self.navToAuthVC()
-        }else{
-            self.navToOnboardingVC()
-        }
-    }
-    
+class OnboardingViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var signupbutton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("loaded onboarding vc")
-        AuthManager.onAuthStateChange(run: self)
         // Do any additional setup after loading the view.
-    }
-    
-    private func navToAuthVC() {
-        guard let rootAuthController = storyboard?.instantiateViewController(identifier: StoryboardId.AuthTabNavigationController.rawValue) else {
-            return
-        }
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.setRootViewController(controller: rootAuthController)
-    }
-    
-    private func navToOnboardingVC() {
-        guard let onboardingVC = storyboard?.instantiateViewController(identifier: StoryboardId.OnboardingNavigationController.rawValue) else {
-            return
-        }
-        (UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate)?.setRootViewController(controller: onboardingVC)
     }
 
 }
--- MealSnap/ViewControllers/SettingsViewController.swift
+++ MealSnap/ViewControllers/SettingsViewController.swift
@@ -16,7 +16,7 @@ class SettingsViewController: UIViewController {
     }
     
     @IBAction func handleLogout() {
-        AuthManager.logout()
+        AuthManager.logout(){_ in }
     }
 
     /*
