52e2fcbe98c4989bd9d8c2fc8cdcb4aa8297adfd
Asif Shikder <shikdernyc@gmail.com>
2021-04-13 03:25:00 -0400

    Handling adding new post

--- MealSnap/ViewControllers/CreatePostViewController.swift
+++ MealSnap/ViewControllers/CreatePostViewController.swift
@@ -12,6 +12,7 @@ class CreatePostViewController: UIViewController {
     @IBOutlet weak var titleInput: UITextField!
     @IBOutlet weak var descriptionTextView: UITextView!
     @IBOutlet weak var imagePreviewImageView: UIImageView!
+    @IBOutlet weak var errorMessageLabel: UILabel!
     
     func styleInput(view: UIView) {
         view.layer.borderWidth = 0.3
@@ -29,9 +30,66 @@ class CreatePostViewController: UIViewController {
         self.openImagePicker()
     }
     
+    private func onPostSuccessful() {
+        DispatchQueue.main.async {
+            guard let userGalleryVC = self.storyboard?.instantiateViewController(identifier: StoryboardId.GalleryViewController.rawValue) else {
+                self.showError(message: "Unable to navigate to user gallery")
+                return
+            }
+            self.navigationController?.pushViewController(userGalleryVC, animated: true)
+        }
+    }
+    
     @IBAction func handleSave() {
         print("Trying to save")
-        let imageData = imagePreviewImageView.image?.jpegData(compressionQuality: 1)?.base64EncodedString()
+        self.closeErrorMessage()
+        guard let targetImage = imagePreviewImageView.image else{
+            self.showError(message: "Please select an image")
+            return
+        }
+        guard titleInput.text?.count != 0 else {
+            self.showError(message: "Title is required")
+            return
+        }
+        guard descriptionTextView.text.count != 0 else {
+            self.showError(message: "Description is required")
+            return
+        }
+        guard let imageData = targetImage.jpegData(compressionQuality: 1) else {
+            showError(message: "Something went wrong")
+            print("Unable to convert image to Data")
+            return
+        }
+        UserGallery.AddImage(title: titleInput.text!, description: descriptionTextView.text!, imageData: imageData) { result in
+            switch (result){
+                case .success(let galleryImage):
+                    print(galleryImage)
+                    self.onPostSuccessful()
+                    return
+                case .failure(let error):
+                    switch(error){
+                    case .APIError(let message):
+                        self.showError(message: message)
+                        return
+                    }
+            }
+        }
+    }
+    
+    func showError(message: String) -> Void {
+        DispatchQueue.main.async {
+            print("Showing error message")
+            self.errorMessageLabel.text = message
+            self.errorMessageLabel.isHidden = false
+        }
+    }
+    
+    func closeErrorMessage() -> Void {
+        DispatchQueue.main.async {
+            if self.errorMessageLabel.isHidden == false {
+                self.errorMessageLabel.isHidden = true
+            }
+        }
     }
     
     func openImagePicker() {
