a157816f5bab637f11d942d6409400fbc34b7cc1
Asif Shikder <shikdernyc@gmail.com>
2021-04-28 16:09:11 -0400

    Added Pagination as common service

---
+++ MealSnap/Services/Common/MealSnapAPIPageResponse.swift
@@ -0,0 +1,12 @@
+//
+//  MealSnapAPIPageResponse.swift
+//  MealSnap
+//
+//  Created by Asif Shikder on 4/27/21.
+//
+
+import Foundation
+
+struct MealSnapAPIPageResponse : Decodable {
+    let next: String?
+}
