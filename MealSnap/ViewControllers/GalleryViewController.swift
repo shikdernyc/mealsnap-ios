7499d816499accead2812097a068a6ec5938b2b5
Asif Shikder <shikdernyc@gmail.com>
2021-04-28 16:11:26 -0400

    Handling updating gallery view with real images

--- MealSnap/ViewControllers/GalleryViewController.swift
+++ MealSnap/ViewControllers/GalleryViewController.swift
@@ -9,28 +9,71 @@ import UIKit
 
 class GalleryViewController: UIViewController {
     public static let StoryboardId = "gallery_vc"
-
+    @IBOutlet weak var galleryTableView: UITableView!
+    
+    private var userGallery: UserGallery?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        galleryTableView.register(GalleryImageTableViewCell.nib(), forCellReuseIdentifier: GalleryImageTableViewCell.ID)
+        galleryTableView.delegate = self
+        galleryTableView.dataSource = self
+        loadInitialData()
+    }
+    
+    func reloadGallery() {
+        DispatchQueue.main.async {
+            self.galleryTableView.reloadData()
+        }
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-        
-    @objc private func handleSelectCreatePost() {
-        print("Selected create post")
+    func loadInitialData() -> Void {
+        UserGallery.GetGalleryForUser(userId: "8b8b14a9-b180-4116-a73c-b6dc5a5fb291"){result in
+            switch(result){
+            case .success(let gallery):
+                self.userGallery = gallery
+                self.reloadGallery()
+            case .failure(let error):
+                print(error)
+            }
+        }
     }
     
-    @objc private func handleSelectSearchUser() {
-        print("Selected search user")
+    func loadMoreData() {
+        if self.userGallery?.canFetchMore() == true {
+            print("Loading More data")
+            self.userGallery?.loadMore() { result in
+                switch(result){
+                case .success:
+                    self.reloadGallery()
+                case .failure(let error):
+                    print(error)
+                }
+                
+            }
+        }
     }
+}
 
+extension GalleryViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("selected")
+    }
+}
+
+extension GalleryViewController : UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        print("Retrieving Cell")
+        let cell = tableView.dequeueReusableCell(withIdentifier: GalleryImageTableViewCell.ID, for: indexPath) as! GalleryImageTableViewCell
+        print(cell)
+//        let cell = tableView.dequeueReusableCell(withIdentifier: UserGalleryTableViewCell.identifier, for: indexPath) as! UserGalleryTableViewCell
+        cell.configure(with: userGallery!.items()[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return userGallery?.items().count ?? 0
+    }
 }
