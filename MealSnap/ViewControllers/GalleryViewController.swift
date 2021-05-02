d9e5f32d5e1c1df6250ca01e8ca5850af5341bce
Asif Shikder <shikdernyc@gmail.com>
2021-05-02 17:33:16 -0400

    Handling pagniation and retrieving image for current user. Added refresh control for refreshing page

--- MealSnap/ViewControllers/GalleryViewController.swift
+++ MealSnap/ViewControllers/GalleryViewController.swift
@@ -13,29 +13,47 @@ class GalleryViewController: UIViewController {
     
     private var userGallery: UserGallery?
     
+    private var userId: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if userId.count == 0 {
+            do {
+                let user = try AuthManager.CurrentUser()
+                self.userId = user.userId
+                print(user)
+            } catch let error {
+                print(error)
+            }
+        }
         galleryTableView.register(GalleryImageTableViewCell.nib(), forCellReuseIdentifier: GalleryImageTableViewCell.ID)
         galleryTableView.delegate = self
         galleryTableView.dataSource = self
-        loadInitialData()
+        galleryTableView.refreshControl = UIRefreshControl()
+        galleryTableView.refreshControl?.addTarget(self, action: #selector(loadInitialData), for: .valueChanged)
+        self.loadInitialData()
     }
     
-    func reloadGallery() {
+    func reloadGalleryData() {
         DispatchQueue.main.async {
             self.galleryTableView.reloadData()
         }
     }
-    
 
-    func loadInitialData() -> Void {
-        UserGallery.GetGalleryForUser(userId: "8b8b14a9-b180-4116-a73c-b6dc5a5fb291"){result in
+    @objc func loadInitialData() -> Void {
+        DispatchQueue.main.async {
+            self.galleryTableView.refreshControl?.beginRefreshing()
+        }
+        UserGallery.GetGalleryForUser(userId: self.userId){result in
             switch(result){
             case .success(let gallery):
                 self.userGallery = gallery
-                self.reloadGallery()
+                self.reloadGalleryData()
             case .failure(let error):
                 print(error)
+            }
+            DispatchQueue.main.async {
+                self.galleryTableView.refreshControl?.endRefreshing()
             }
         }
     }
@@ -46,7 +64,7 @@ class GalleryViewController: UIViewController {
             self.userGallery?.loadMore() { result in
                 switch(result){
                 case .success:
-                    self.reloadGallery()
+                    self.reloadGalleryData()
                 case .failure(let error):
                     print(error)
                 }
@@ -59,21 +77,39 @@ class GalleryViewController: UIViewController {
 extension GalleryViewController : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        print("selected")
     }
 }
 
 extension GalleryViewController : UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("Retrieving Cell")
         let cell = tableView.dequeueReusableCell(withIdentifier: GalleryImageTableViewCell.ID, for: indexPath) as! GalleryImageTableViewCell
-        print(cell)
-//        let cell = tableView.dequeueReusableCell(withIdentifier: UserGalleryTableViewCell.identifier, for: indexPath) as! UserGalleryTableViewCell
         cell.configure(with: userGallery!.items()[indexPath.row])
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return userGallery?.items().count ?? 0
+    }
+}
+
+extension GalleryViewController {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard userGallery?.canFetchMore() == true else {
+            return
+        }
+        let position = scrollView.contentOffset.y
+        if position > (galleryTableView.contentSize.height - 600 - scrollView.frame.size.height){
+                print("Loading More")
+                userGallery?.loadMore() { result in
+                    switch(result){
+                    case .success:
+                        self.reloadGalleryData()
+                        return
+                    case .failure(let error):
+                        print("Failed to load more")
+                        print(error)
+                    }
+                }
+        }
     }
 }
