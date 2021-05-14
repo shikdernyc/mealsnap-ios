a8737184ac23ff92ac28917c25b00031b7b01440
Asif Shikder <shikdernyc@gmail.com>
2021-05-13 20:10:27 -0400

    Added Image Only Gallery Table Cell. Added AppPreference class

---
+++ MealSnap/Components/ImageOnlyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ImageOnlyTableViewCell.swift
+//  MealSnap
+//
+//  Created by Asif Shikder on 5/13/21.
+//
+
+import UIKit
+
+class ImageOnlyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ MealSnap/Models/Preference/AppPreference.swift
@@ -0,0 +1,8 @@
+//
+//  AppPreference.swift
+//  MealSnap
+//
+//  Created by Asif Shikder on 5/13/21.
+//
+
+import Foundation
