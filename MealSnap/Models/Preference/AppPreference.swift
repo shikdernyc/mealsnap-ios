48f060202803f006ebdc8765ee66375f7dfef1f8
Asif Shikder <shikdernyc@gmail.com>
2021-05-13 20:11:42 -0400

    Added Hide Image Detail preference

--- MealSnap/Models/Preference/AppPreference.swift
+++ MealSnap/Models/Preference/AppPreference.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+
+typealias OnChangePreferenceListener<T> = (_ newValue : T) -> Void
+
+class UserPreference {
+    private static let HIDE_IMAGE_DETAIL_KEY = "HIDE_IMAGE_DETAIL"
+    private static var HideImageDetailListender : [OnChangePreferenceListener<Bool>] = []
+    
+    static func HideImageDetail() -> Bool {
+        return UserDefaults.standard.bool(forKey: UserPreference.HIDE_IMAGE_DETAIL_KEY)
+    }
+    
+    static func HideImageDetail(onChange: @escaping OnChangePreferenceListener<Bool>) -> Void {
+        UserPreference.HideImageDetailListender.append(onChange)
+    }
+    
+    static func HideImageDetail(set to: Bool) {
+        UserDefaults.standard.set(to, forKey: HIDE_IMAGE_DETAIL_KEY)
+        for listender in HideImageDetailListender {
+            listender(to)
+        }
+    }
+}
